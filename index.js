aa2b97217b191bc55fd48697ccaac8f59bab7c74
Roman Oliinyk <oliinyk@oliinyk.local>
2019-10-01 11:23:50 +0300

    updated code to dynamically adapt to change in parameters

--- index.js
+++ index.js
@@ -58,61 +58,53 @@ var params = {
   ]
 };
 
-const lineWidth = window.innerWidth;
-const lineHeight = window.innerHeight / params.lines.length;
-const updateTimerTick = params.lines[0].updateTime;
-let line;
-let elementOfLine;
-let elementWidth;
-// adding lines on page
-let num = params.lines.length;
-for (let i = 1; i <= num; i++) {
-  line = document.createElement("div");
-  line.classList.add("line-container");
-  line.style.display = "flex";
-  line.style.flexFlow = "row wrap";
-  line.style.width = lineWidth + "px";
-  line.style.height = lineHeight + "px";
-  line.style.background = params.lines[0].background;
-  document.body.appendChild(line);
-  //adding inner elements
-  for (let j = 0; j < params.lines[0].elements.length; j++) {
-    params.lines[0].elements[j].width % 2 !== 0
-      ? (elementWidth = 25 + "%")
-      : (elementWidth = 50 + "%");
-    elementOfLine = document.createElement("div");
-    elementOfLine.classList.add("line-element");
-    elementOfLine.style.width = elementWidth;
-    elementOfLine.style.height = line.style["height"];
-    for (let k = 0; k < params.lines; k++) {
-      for (let l = 0; l < params.lines[k].background; l++) {
-        elementOfLine.style.background = params.lines[k].elements[l].background;
-      }
-    }
-    elementOfLine.style.transition = "all 0.5s ease-out";
-    line.appendChild(elementOfLine);
-  }
-}
-
-// Color generator
-generateColor = () => {
+function generateColor() {
   return (
     "#" +
     Math.random()
       .toString(16)
       .slice(2, 8)
   );
-};
+}
 
-// Background color function
-setBackground = () => {
-  const element = document.getElementsByClassName("line-element");
-  for (let i = 0; i < element.length; i++) {
-    element[i].style.background = generateColor();
-  }
-};
+const lineWidth = window.innerWidth;
+const lineHeight = window.innerHeight / params.lines.length + "px";
+const view = [];
 
-// Starting function on page load
-document.body.onload = () => {
-  setInterval(setBackground, updateTimerTick);
-};
+function buildLine(line, randomColor) {
+  var lineHTML = document.createElement("div");
+
+  lineHTML.style.width = line.width ? line.width + "%" : lineWidth;
+  lineHTML.style.height = lineHeight;
+
+  lineHTML.style.backgroundColor = randomColor
+    ? generateColor()
+    : line.background;
+
+  return lineHTML;
+}
+
+function paintLine(line, randomColor) {
+  var parentLine = buildLine(line, randomColor);
+
+  line.elements
+    .map(function(innerLine) {
+      return buildLine(innerLine, randomColor);
+    })
+    .forEach(function(lineHTML) {
+      parentLine.appendChild(lineHTML);
+      lineHTML.style.float = "left";
+    });
+
+  return parentLine;
+}
+
+params.lines.forEach(function(line, index) {
+  var parentLine = paintLine(line);
+
+  document.body.appendChild(parentLine);
+
+  setInterval(function() {
+    parentLine.innerHTML = paintLine(line, true).innerHTML;
+  }, line.updateTime);
+});
